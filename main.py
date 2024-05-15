eb13a1c2a8b55c1f3b556a43ea0d4e434ab47a83
ZFengde <feng.1.zhang@kcl.ac.uk>
2024-05-15 11:51:08 +0100

    env_id update

--- main.py
+++ main.py
@@ -43,13 +43,13 @@ def main(
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env_id', type=str, default='Ant-v4') 
+    parser.add_argument('--env_id', type=str, default='Humanoid-v4') 
     parser.add_argument('--algo', type=str, default='Diff_TD3') 
     parser.add_argument('--n_envs', type=int, default=6)
     parser.add_argument('--iter_num', type=int, default=5) # Total_timestep = iter_num * n_envs * n_steps, here is 2000 * 4 * 20480 = 1.2e7
     parser.add_argument('--seed', type=int, default=4)
     parser.add_argument('--learning_rate', type=int, default=1e-4)
-    parser.add_argument('--n_sampled_actions', type=int, default=1)
+    parser.add_argument('--n_sampled_actions', type=int, default=10)
     args = parser.parse_args()
 
     main(
